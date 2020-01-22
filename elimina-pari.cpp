7f21d0e425f58095524866d285ee4b364bdc9bd9
Francesco Musi <37630916+FrancescoMusi@users.noreply.github.com>
2020-01-22 10:04:58 +0100

    Update elimina-pari.cpp

--- elimina-pari.cpp
+++ elimina-pari.cpp
@@ -2,25 +2,25 @@
 #include <assert.h>
 
 int main() {
-	int N, max, min, i;
+	int n, max, min, i;
 	
 	freopen("input/3", "r", stdin);
 	freopen("output/3", "w", stdout);
 	
-	assert( 1 == scanf("%d", &N) );
+	assert( 1 == scanf("%d", &n) );
 
-	int lista[N];
+	int l[N];
 	
-	for( i = 0; i < N; i++) {
-		assert( 1 == scanf("%d", &lista[i]) );
+	for( i = 0; i < n; i++) {
+		assert( 1 == scanf("%d", &l[i]) );
 	}
 	
-	max = lista[0];
-	min = lista[0];
+	max = l[0];
+	min = l[0];
 	
-	for( i = 0; i < N; i++) {
-		if (lista[i] % 2 != 0) {			
-			printf("%d ", lista[i]);
+	for( i = 0; i < n; i++) {
+		if (l[i] % 2 != 0) {			
+			printf("%d ", l[i]);
 		};
 	}	
 	
